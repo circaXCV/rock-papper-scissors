fac26a3ef753ba62a094f3a5f6ecb52781d13ecc
whomstah <alexi.who@gmail.com>
2021-11-20 12:15:43 -0500

    Fixed userPlay() so it returns valid RPS selection converted to lowercase. Added additional console log and alert messages. Added win/loss/tie count.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 /* Rock = 0 Paper = 1 Scissors = 2 */
 let rockPaperScissors = ['rock', 'paper', 'scissors'];
+let playerSelection = "";
+let computerSelection = "";
+
+/* Starting values for wins, loses, and ties */
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
 /* Select rock paper or scissors at random */ 
 function computerPlay() {
-    let computerSelection =  rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
-
+    computerSelection =  rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
     return computerSelection;
 }
 
@@ -12,10 +18,12 @@ function computerPlay() {
 function userPlay() {
     playerSelection = window.prompt("Rock, paper, or scissors, which will it be?");
     if (rockPaperScissors.indexOf(playerSelection.toLowerCase()) !== -1) {
-        return playerSelection.toLowerCase();
+        playerSelection = playerSelection.toLowerCase();
+        return playerSelection;
 
     /* Prompt repeats if input is not rock, paper, or scissors */
     } else {
+        alert("Not a valid entry, please try again.");
         userPlay();
     }
 }
@@ -27,29 +35,36 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         console.log("You played: " + playerSelection);
         console.log("Computer played: " + computerSelection);
+        console.log("It's a tie.");
         alert("You played: " + playerSelection + ". \nThe computer played: " + computerSelection + ". \n It's a tie.")
-        return ;
+        return ties++;
 
     /* Logic for player win */
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
         console.log("You played: " + playerSelection);
         console.log("Computer played: " + computerSelection);
+        console.log("You win!");
         alert("You played: " + playerSelection + ". \nThe computer played: " + computerSelection + ". \n You win!")
-        return ;
+        return wins++;
 
     /* Logic for player lose */
     } else {
         console.log("You played: " + playerSelection);
         console.log("Computer played: " + computerSelection);
+        console.log("You lose.");
         alert("You played: " + playerSelection + ". \nThe computer played: " + computerSelection + ". \n Better luck next time.");
-        return ;
+        return losses++;
     }
 }
-
+/* For loop that calls playRound() five times and opens a window displaying the results. */
 function playMatch() {
     for (let i = 0; i < 5; i++) {
-        playRound(userPlay(), computerPlay());
+        userPlay();
+        computerPlay();
+        playRound(playerSelection, computerSelection);
     }
+    console.log("Final score: " + wins + "win(s)" + " - " + losses + "loss(es)" + " - " + ties + "tie(s)");
+    alert("Thanks for playing! Your final score is " + wins + " win(s) to " + losses + " loss(es) and " + ties + " tie(s).");
 }
 
 playMatch();
